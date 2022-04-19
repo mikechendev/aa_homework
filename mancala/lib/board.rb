e0e43627a0312c730a6c06bbf48a191ebad18d33
Michael Chen <michael.chen.1120@gmail.com>
2022-04-18 22:11:26 -0400

    Add mancala

---
+++ mancala/lib/board.rb
@@ -0,0 +1,82 @@
+class Board
+  attr_accessor :cups
+
+  def initialize(name1, name2)
+    @name1 = name1
+    @name2 = name2
+    @cups = Array.new(14) {Array.new}
+    place_stones
+  end
+
+  def place_stones
+    # helper method to #initialize every non-store cup with four stones each
+    (0...@cups.length).each do |i|
+      if i == 6 || i == 13
+        next
+      else
+        4.times do
+          @cups[i] << :stone
+        end
+      end
+    end
+  end
+
+  def valid_move?(start_pos)
+    raise 'Invalid starting cup' if !start_pos.between?(0, 13)
+    raise 'Starting cup is empty' if @cups[start_pos].empty?
+  end
+
+  def make_move(start_pos, current_player_name)
+    stones = @cups[start_pos]
+    @cups[start_pos] = []
+
+    cup_idx = start_pos
+    while !stones.empty?
+      cup_idx += 1
+      cup_idx = 0 if cup_idx > 13
+
+      if cup_idx == 6
+        @cups[6] << stones.pop if current_player_name == @name1
+      elsif cup_idx == 13
+        @cups[13] << stones.pop if current_player_name == @name2
+      else
+        @cups[cup_idx] << stones.pop
+      end
+    end
+    render
+    next_turn(cup_idx)
+  end
+
+  def next_turn(ending_cup_idx)
+    # helper method to determine whether #make_move returns :switch, :prompt, or ending_cup_idx
+    if ending_cup_idx == 6 || ending_cup_idx == 13
+      :prompt
+    elsif @cups[ending_cup_idx].count == 1
+      :switch
+    else
+    ending_cup_idx
+    end
+  end
+
+  def render
+    print "      #{@cups[7..12].reverse.map { |cup| cup.count }}      \n"
+    puts "#{@cups[13].count} -------------------------- #{@cups[6].count}"
+    print "      #{@cups.take(6).map { |cup| cup.count }}      \n"
+    puts ""
+    puts ""
+  end
+
+  def one_side_empty?
+    cups[0..5].all? { |cup| cup.empty? } || cups[7..12].all? { |cup| cup.empty? }
+  end
+
+  def winner
+    return :draw if cups[6].length == 6 && cups[13].length == 6
+    if cups[6].length == 6
+      return @name1
+    end
+    if cups[13].length == 6
+      return @name2
+    end
+  end
+end
