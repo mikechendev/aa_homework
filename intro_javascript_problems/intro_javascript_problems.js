a1f9b7ebe0582185ef1b3b23b2c18f80d0a8e10c
Michael Chen <michael.chen.1120@gmail.com>
2022-05-17 22:48:08 -0400

    Add js pt 2

--- intro_javascript_problems/intro_javascript_problems.js
+++ intro_javascript_problems/intro_javascript_problems.js
@@ -1,5 +1,5 @@
 const madLib = (verb, adj, noun) => {
-    return console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`)
+    return console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`);
 };
 
 //> madLib('make', 'best', 'guac');
