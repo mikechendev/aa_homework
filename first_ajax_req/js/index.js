f44610d79e9b12d9686cf938e67a8feb1cf8f7ef
Michael Chen <michael.chen.1120@gmail.com>
2022-05-25 11:22:58 -0400

    Finish ajax hw

--- first_ajax_req/js/index.js
+++ first_ajax_req/js/index.js
@@ -1,5 +1,20 @@
 console.log("Hello from the JavaScript console!");
 
 // Your AJAX request here
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?q=new%20york,US&appid=bcb83c4b54aee8418983c2aff3073b3b",
+    type: "get",
+    dataType: "json",
+    success: (data) => {
+        let temp = data["main"]["temp"]
+
+        console.log(temp);
+        console.log("worked!");
+    },
+    error: () => {
+        console.log("did not work!");
+    }
+})
 
 // Add another console log here, outside your AJAX request
+console.log('this is outside the AJAX request');
